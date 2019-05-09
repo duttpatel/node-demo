94929fa93e686885ff15834ba41aea0c66c012ff
Dutt Patel <Dutt.Patel@rci.rogers.ca>
2019-05-09 16:52:27 -0400

    first server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const http = require('http');
+
+// function listner(req,res){
+
+// }
+
+// http.createServer(listner);
+
+const server = http.createServer(function (req,res) {
+
+});
+
+server.listen(3000)
